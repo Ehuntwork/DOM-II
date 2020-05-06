371dd7e8faee60205fdf100b0f2847e43aacc9a5
emilme <emilme@sir.mle>
2020-05-06 14:06:01 -0700

    Completed 4/10 eventListeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+let busImg = document.querySelector(".intro img");
+busImg.addEventListener('mouseenter', () =>{
+    busImg.style.transform = 'scale(1.3)';
+    busImg.style.transition = 'transform .5s';
+})
+busImg.addEventListener('mouseout', () =>{
+    busImg.style.transform = 'scale(1)';
+})
+
+
+let allImgs = document.querySelectorAll('img');
+allImgs.forEach((element) => 
+    element.addEventListener('click', (event) => {
+    event.target.style.transform = 'rotate(360deg)';
+    event.target.style.transition = 'transform .5s';
+}))
+
+
+let allH2 = document.querySelectorAll('h2');
+allH2.addEventListener('keydown', (event) => {
+    event.target.style.display = 'none';
+}
+)
+
+
+/* [ ] `mouseover`
+* [ ] `keydown`
+* [ ] `wheel`
+* [ ] `drag / drop`
+* [ ] `load`
+* [ ] `focus`
+* [ ] `resize`
+* [ ] `scroll`
+* [ ] `select`
+* [ ] `dblclick`*/
